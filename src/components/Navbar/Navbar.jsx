679dd78c58eff6cc28607008d3f6f2ed35038c65
emilia_vv <emilka.wojcik2002@gmail.com>
2025-01-01 14:31:23 +0100

    first draft of the main site

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Navbar.css";
+import Logo from "../../assets/Logo.png";
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="Main">
+        <ul>
+          <li className="company">
+            <img src={Logo} alt="logo" width="60px" />
+            <h1>
+              <a href="#">Berry</a>
+            </h1>
+          </li>
+          <li>
+            <a href="#">Sign In / Register</a>
+          </li>
+        </ul>
+      </div>
+      <div className="Nav">
+        <ul>
+          <li>
+            <a href="#">Just In</a>
+          </li>
+          <li>
+            <a href="#">Designer</a>
+          </li>
+          <li>
+            <a href="#">Dresses</a>
+          </li>
+          <li>
+            <a href="#">Bags</a>
+          </li>
+          <li>
+            <a href="#">Sale</a>
+          </li>
+          <li>
+            <form>
+              <input type="text" placeholder="🔎 Search..." />
+            </form>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
